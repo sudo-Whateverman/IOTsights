120914f912d57584f308bb5df1fca616553aa8cb
sudo-Whateverman <nick@smartgreen.co.il>
2016-03-29 06:42:49 +0300

    added markers amd lines that point out to heading (NOT WORKING)

--- commander/views.py
+++ commander/views.py
@@ -3,11 +3,12 @@ from .models import Shot
 # Create your views here.
 
 def shots_list(request):
-    shots = Shot.objects.order_by('registered_date')
+    shots = Shot.objects.order_by('-registered_date')
     return render(request, 'shots_list.html', {'shots': shots})
 
 def shots_map(request):
-    shots = Shot.objects.order_by('registered_date')
-    return render(request, 'shots_map.html', {'shots': shots})
+    shots = Shot.objects.order_by('-registered_date')
+    center = Shot.objects.order_by('-registered_date')[0]
+    return render(request, 'shots_map.html', {'shots': shots, 'center': center})
 
 
--- templates/simple_map.js
+++ templates/simple_map.js
@@ -1,4 +1,15 @@
-var map;
+// This example adds a marker to indicate the position of Bondi Beach in Sydney,
+// Australia.
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), { center: {lat: -34.397, lng: 150.644}, zoom: 8 });
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: {lat: -33, lng: 151}
+  });
+
+  var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+  var beachMarker = new google.maps.Marker({
+    position: {lat: -33.890, lng: 151.274},
+    map: map,
+    icon: image
+  });
 }
