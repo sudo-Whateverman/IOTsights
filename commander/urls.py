bc73f3dcf90506448e6cbc7e8cce483c15086f4c
sudo-Whateverman <nick@smartgreen.co.il>
2016-04-11 22:15:35 +0300

    added a GET view

--- commander/urls.py
+++ commander/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^map/', views.shots_map, name='shots_map'),
     url(r'^add/', views.shot_new, name='add'),
     url(
-        r'^add/(?P<iot_id>[0-9]+)/(?P<lat>[0-9]+)/(?P<longt>[0-9]+)/(?P<elev>[0-9]+)/(?P<head>[0-9]+)$',
+        r'^get/(?P<iot_id>[0-9]+)/(?P<lat>[0-9]+)/(?P<longt>[0-9]+)/(?P<elev>[0-9]+)/(?P<head>[0-9]+)$',
         views.shot_new_get, name='add_get'),
 ]
