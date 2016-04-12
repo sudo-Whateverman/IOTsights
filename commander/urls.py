299a7ba64a682c762f5e341b9ff535bcf2fe69b5
root <root@ip-172-31-43-124.eu-west-1.compute.internal>
2016-04-12 19:20:42 +0000

    changes added on aws

--- commander/urls.py
+++ commander/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from . import views
 
+re_ = "(-?\d+(?:\.\d+)?)"
+
 urlpatterns = [
     url(r'^$', views.shots_list, name='shots_list'),
     url(r'^map/', views.shots_map, name='shots_map'),
     url(r'^add/', views.shot_new, name='add'),
     url(
-        r'^get/(?P<iot_id>[0-9]+)/(?P<lat>[0-9]+)/(?P<longt>[0-9]+)/(?P<elev>[0-9]+)/(?P<head>[0-9]+)$',
+        r'^get/(?P<iot_id>'+re_+')/(?P<lat>'+re_+')/(?P<longt>'+re_+')/(?P<elev>'+re_+')/(?P<head>'+re_+')$',
         views.shot_new_get, name='add_get'),
 ]
